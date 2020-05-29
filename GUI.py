6c0bbfabdd3738c910bcbf27d14c54d5604d23ea
Robel1992 <Robel1992@gmail.com>
2020-05-29 13:32:27 -0500

    GUI update -- calculator finished

--- GUI.py
+++ GUI.py
@@ -214,50 +214,91 @@ master.title("Shitty Calculator")
 #framework
 
 #Print Answer
-def printAnswer():
-    label = Label(master, text= "Here's the answer")
-    label.grid(row=1, column = 4)
+def function_class(string_var):
+    label = Label(master, text= string_var)
+    label.grid(row=0, column = 1)
 
 
+def add():
+    sum = int(first_number.get())+int(second_number.get())
+    print(type(sum))
+    function_class("+")
+    label = Label(master, text= sum)
+    label.grid(row=0, column = 4)
+
+def sub():
+    diff = int(first_number.get())-int(second_number.get())
+    print(type(diff))
+    function_class("-")
+    label = Label(master, text= diff)
+    label.grid(row=0, column = 4)
+
+def mul():
+    
+    prod = int(first_number.get())*int(second_number.get())
+    print(type(prod))
+    function_class("x")
+    label = Label(master, text= prod)
+    label.grid(row=0, column = 4)
+
+def div():
+    if second_number.get() == "0":
+        function_class("÷")
+        label=Label(master, text="ERROR: DIVISION BY ZERO")
+        label.grid(row=0, column = 4)
+
+    quot = int(first_number.get())/int(second_number.get())
+    print(type(quot))
+    function_class("÷")
+    label = Label(master, text= quot)
+    label.grid(row=0, column = 4)
+
+def clear_table():
+    label=Label(master, text="                                                ")
+    label.grid(row=0, column = 4)
 
 #Turn first number and second number into a string
-first_number_val = StringVar()
-second_number_val = StringVar()
+#first_number_val = StringVar()
+#second_number_val = StringVar()
 
 #the label of the box
 label_1 = Label(master, text = "First Number")
 label_2 = Label(master, text = "Second Number")
 label_answer = Label(master, text = "The answer")
-first_number = Entry(master, textvariable = first_number_val, width=5)
-second_number = Entry(master, textvariable = second_number_val, width=5)
+first_number = Entry(master, width=5)
+second_number = Entry(master, width=5)
 
 
 
 #placeing the elements we created ABOVE!
-label_1.grid(row=2, column=0)
-label_2.grid(row=2, column=2)
-label_answer.grid(row=2, column = 4)
+label_1.grid(row=1, column=0)
+label_2.grid(row=1, column=2)
+label_answer.grid(row=1, column = 4)
 
-first_number.grid(row=1, column=0)
-second_number.grid(row=1, column=2)
+first_number.grid(row=0, column=0)
+second_number.grid(row=0, column=2)
 
 
 #Buttons
-Button_add = Button(master, text="+", width=2) 
-Button_add.grid(row=0, column=1)
+Button_add = Button(master, text="+", width=2, command = add) 
+Button_add.grid(row=2, column=0)
 
-Button_sub = Button(master, text="-", width=2)
-Button_sub.grid(row=1, column=1)
+Button_sub = Button(master, text="-", width=2, command = sub)
+Button_sub.grid(row=2, column=1)
 
-Button_mul = Button(master, text="*", width=2)
-Button_mul.grid(row=2, column=1)
+Button_mul = Button(master, text="x", width=2, command = mul)
+Button_mul.grid(row=3, column=0)
 
 
-Button_div = Button(master, text="/", width=2)
+Button_div = Button(master, text="÷", width=2, command = div)
 Button_div.grid(row=3, column=1)
 
-Button_equals = Button(master, text="=", width=3, command = printAnswer)
-Button_equals.grid(row = 1 , column=3)
+Button_equals = Button(master, text="=", width=3)
+Button_equals.grid(row = 0, column=3)
+
+Button_clear = Button(master, text="c", width =3, command=clear_table)
+Button_clear.grid(row = 2 , column=2)
+
 
 #master.geometry("500x500")
 
